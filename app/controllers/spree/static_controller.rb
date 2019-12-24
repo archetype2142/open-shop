3b73c4f752a221387c78aafb7613e9f1c89d90d0
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-24 11:55:12 +0300

    logging puts

--- app/controllers/spree/static_controller.rb
+++ app/controllers/spree/static_controller.rb
@@ -3,6 +3,8 @@ class Spree::StaticController < Spree::HomeController
   def barcode
     product = Spree::Product.find_by!(barcode: params[:barcode])
     
+    puts "\n\n\n\nBarcode scanned: #{params[:barcode]}"
+    
     if !product 
       redirect_to spree.scanner_path, flash[:error] = "Cant' find product"
     else
