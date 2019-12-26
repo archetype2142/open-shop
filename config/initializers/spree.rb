a956cd5d0de92fb0ce45a0116567a5b5e496686b
archetype2142 <malhotraritwick2011@gmail.com>
2019-12-26 05:39:41 +0530

    done

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -16,6 +16,26 @@ Spree.config do |config|
   config.logo = 'logo.png'
   config.admin_interface_logo = 'logo.png'
   config.always_put_site_name_in_title = false
+  attachment_config = {
+    s3_credentials: {
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      bucket:            ENV['S3_BUCKET_NAME']
+    },
+
+    storage:        :s3,
+    s3_region:      ENV['S3_REGION'],
+    s3_headers:     { "Cache-Control" => "max-age=31557600" },
+    s3_protocol:    "https",
+    bucket:         ENV['S3_BUCKET_NAME'],
+
+    path:           "/:class/:id/:style/:basename.:extension",
+    default_url:    "/:class/:id/:style/:basename.:extension",
+  }
+
+  attachment_config.each do |key, value|
+    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  end
 end
 
 # Configure Spree Dependencies
